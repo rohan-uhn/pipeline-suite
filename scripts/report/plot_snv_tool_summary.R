9537cdb9314eaf5abb269f4db773b03dac0b617f
rohan-uhn <RohanAhmed.Khan@uhn.ca>
2025-06-11 11:17:03 -0400

    Update plot_snv_tool_summary.R

--- scripts/report/plot_snv_tool_summary.R
+++ scripts/report/plot_snv_tool_summary.R
@@ -216,6 +216,9 @@ plot.objects[['overlap']] <- create.barplot(
 		)			
 	);
 
+print(names(plot.objects))
+print(length(plot.objects))
+print(length(c(rep(1,tool.count), 2.5)))
 # combine them!
 create.multipanelplot(
 	plot.objects = plot.objects,
